6dbd6999e4afd5d19ec46d02e029db15f7fff19f
tio <tio@codecoffeecreative.com>
2024-03-25 16:30:35 +0700

    adjust height image

--- src/components/CarsCard/CarsCard.tsx
+++ src/components/CarsCard/CarsCard.tsx
@@ -8,7 +8,7 @@ const CarsCard = ({
   return (
     <div className='md:basis-1/4 w-full rounded-xl md:px-3 cursor-pointer mt-5'>
       <div className='bg-white shadow-lg'>
-        <img src={require('assets/img/cars-logo.png')} alt="article-img" className='w-full md:h-24 h-42 object-fit justify-self-center' />
+        <img src={require('assets/img/cars-logo.png')} alt="article-img" className='w-full md:h-22 h-42 object-fit justify-self-center' />
         <div className='p-5 w-full min-h-[130px]'>
           <p className='text-xl line-clamp-1 text elipsis font-bold'>{UpperCaseFirstLetter(car.model)}</p>
           <p className='text-sm line-clamp-1 text elipsis mt-1'>{UpperCaseFirstLetter(car.make)}</p>
